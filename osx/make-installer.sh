4c9fa23a0b7b7f4819c37b03d65e48c2154c063e
Stephen Lyons <slysven@virginmedia.com>
2023-11-24 01:25:42 +0000

    Try again

--- osx/make-installer.sh
+++ osx/make-installer.sh
@@ -151,9 +151,9 @@ python macdeployqtfix.py "${app}/Contents/MacOS/brimworks/zip.so" "/usr/local"
 # symbolic link but when they are copied the linked to file will be what is
 # copied:
 # Copy the right file explicity so we can examine why the wildcard one doesn't work
-cp "/usr/local/Cellar/libzip/1.10.1/lib/libzip.5.5.dylib" "${app}/Contents/Frameworks/libzip.5.dylib"
 ls -l /usr/local/Cellar/libzip/1.*/lib/libzip.5.*.dylib
-cp "/usr/local/Cellar/libzip/1.*/lib/libzip.5.*.dylib" "${app}/Contents/Frameworks/libzip.5.dylib"
+cp "/usr/local/Cellar/libzip/1.10.1/lib/libzip.5.5.dylib" "${app}/Contents/Frameworks/libzip.5.dylib"
+# cp "/usr/local/Cellar/libzip/1.*/lib/libzip.5.*.dylib" "${app}/Contents/Frameworks/libzip.5.dylib"
 ls -l /usr/local/Cellar/xz/5.*/lib/liblzma.5.dylib
 cp "/usr/local/Cellar/xz/5.*/lib/liblzma.5.dylib" "${app}/Contents/Frameworks/liblzma.5.dylib"
 ls -l /usr/local/Cellar/zstd/1.*/lib/libzstd.1.dylib

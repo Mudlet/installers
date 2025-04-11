9b98634476f5aa44e8f9fa820ef484f63f5a0307
Stephen Lyons <slysven@virginmedia.com>
2025-04-11 15:50:09 +0100

    Revise: more changes to get translations, sound and dictionaries working
    Signed-off-by: Stephen Lyons <slysven@virginmedia.com>

--- generic-linux/build-and-make-installer.sh
+++ generic-linux/build-and-make-installer.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+############################################################################
+#    Copyright (C) 2017-2018 by Keneanung <kenenanung@googlemail.com>      #
+#    Copyright (C) 2025 by Stephen Lyons       - <slysven@virginmedia.com> #
+#                                                                          #
+#    This program is free software; you can redistribute it and/or modify  #
+#    it under the terms of the GNU General Public License as published by  #
+#    the Free Software Foundation; either version 2 of the License, or     #
+#    (at your option) any later version.                                   #
+#                                                                          #
+#    This program is distributed in the hope that it will be useful,       #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#    GNU General Public License for more details.                          #
+#                                                                          #
+#    You should have received a copy of the GNU General Public License     #
+#    along with this program; if not, write to the                         #
+#    Free Software Foundation, Inc.,                                       #
+#    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
+############################################################################
 
 # abort script if any command fails
 set -e
@@ -8,8 +27,15 @@ PGM=$(basename "$0")
 
 # Where to put the Mudlet source code:
 if [ -z "${SOURCE_DIR}" ]; then
-  SOURCE_DIR="$(pwd)/work_area"
+  SOURCE_DIR="$(pwd)/working"
   export SOURCE_DIR
+fi
+
+if [ -n "$(echo "${SOURCE_DIR}" | cut -s -d_ -f2- )" ]; then
+  echo "Unfortunately the SOURCE_DIR is: \"${SOURCE_DIR}\" and it contains"
+  echo "one or more '_' characters and this will break something in the"
+  echo "make-install.sh script - aborting..."
+  exit 1
 fi
 
 echo "Working in: '${SOURCE_DIR}'"
@@ -23,7 +49,7 @@ if [ ! -d "${SOURCE_DIR}" ]; then
   # check command line option for commit-ish that should be checked out
   COMMITISH="$1"
   if [ -z "${COMMITISH}" ]; then
-    echo "No 'source' folder exists and no commit-ish given."
+    echo "No 'SOURCE_DIR' environmental variable exists and no commit-ish given."
     echo "Usage: ${PGM} <commit-ish>"
     exit 2
   fi
--- generic-linux/make-installer.sh
+++ generic-linux/make-installer.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+############################################################################
+#    Copyright (C) 2017-2020, by Vadim Peretokin  - <vperetokin@gmail.com> #
+#    Copyright (C) 2017-2020 by Keneanung <kenenanung@googlemail.com>      #
+#    Copyright (C) 2019, 2025 by Stephen Lyons - <slysven@virginmedia.com> #
+#    Copyright (C) 2020 by Edru2                                           #
+#                              - <60551052+Edru2@users.noreply.github.com> #
+#                                                                          #
+#    This program is free software; you can redistribute it and/or modify  #
+#    it under the terms of the GNU General Public License as published by  #
+#    the Free Software Foundation; either version 2 of the License, or     #
+#    (at your option) any later version.                                   #
+#                                                                          #
+#    This program is distributed in the hope that it will be useful,       #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#    GNU General Public License for more details.                          #
+#                                                                          #
+#    You should have received a copy of the GNU General Public License     #
+#    along with this program; if not, write to the                         #
+#    Free Software Foundation, Inc.,                                       #
+#    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
+############################################################################
 
 # abort script if any command fails
 set -e
@@ -7,8 +29,8 @@ RELEASE="false"
 PTB="false"
 
 if [ -n "${GITHUB_REPOSITORY}" ] ; then
-  BUILD_DIR=${BUILD_FOLDER}
   SOURCE_DIR=${GITHUB_WORKSPACE}
+  BUILD_DIR=${BUILD_FOLDER}
 fi
 
 if [ -z "${SOURCE_DIR}" ]; then
@@ -18,6 +40,8 @@ else
   echo "Working with source code in: '${SOURCE_DIR}'".
 fi
 
+# BUILD_DIR wil be ${SOURCE_DIR}/build if this script is run from
+# build-and-make-installer.sh:
 if [ -z "${BUILD_DIR}" ]; then
   echo "BUILD_DIR is not set, aborting.".
   exit 1
@@ -32,7 +56,7 @@ export APP_DIR
 while getopts ":pr:" option; do
   if [ "${option}" = "r" ]; then
     RELEASE="true"
-    VERSION="${OPTARG}"
+    LINUXDEPLOY_OUTPUT_VERSION="${OPTARG}"
     shift $((OPTIND-1))
   elif [ "${option}" = "p" ]; then
     PTB="true"
@@ -43,15 +67,21 @@ while getopts ":pr:" option; do
   fi
 done
 if [ "${RELEASE}" != "true" ]; then
-  VERSION="${1}"
+  LINUXDEPLOY_OUTPUT_VERSION="${1}"
 fi
 
 # The environmental variable VERSION is used by the linuxdeploy process to
-# identify the build.
-export VERSION
+# identify the build - although we do now get warnings to use
+# LINUXDEPLOY_OUTPUT_VERSION instead?
+export LINUXDEPLOY_OUTPUT_VERSION
 
 # setup linuxdeployqt binaries if not found
 if [ "$(getconf LONG_BIT)" = "64" ]; then
+#  if [[ ! -e appImageTool.AppImage ]]; then
+#      # download prepackaged appImageTool - needed (?) to
+#      echo "appImageTool not found - downloading it."
+#      wget -nv -O appImageTool.AppImage https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage
+#  fi
   if [[ ! -e linuxdeploy.AppImage ]]; then
       # download prepackaged linuxdeploy
       echo "linuxdeploy not found - downloading it."
@@ -64,7 +94,6 @@ if [ "$(getconf LONG_BIT)" = "64" ]; then
       wget -nv -O linuxdeploy-plugin-qt.AppImage https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage
       chmod +x linuxdeploy-plugin-qt.AppImage
   fi
-
   if [[ ! -e linuxdeploy-plugin-gstreamer.sh ]]; then
       # download prepackaged linuxdeploy-plugin-gstreamer.
       echo "linuxdeploy-plugin-gstreamer not found - downloading it."
@@ -77,7 +106,7 @@ else
 fi
 
 if [ "${RELEASE}" != "true" ]; then
-  OUTPUT_NAME="Mudlet-${VERSION}"
+  OUTPUT_NAME="Mudlet-${LINUXDEPLOY_OUTPUT_VERSION}"
 else
   if [ "${PTB}" == "true" ]; then
     OUTPUT_NAME="Mudlet PTB"
@@ -91,42 +120,51 @@ echo "APP_DIR is: \"${APP_DIR}\""
 echo "QMAKE is: \"${QMAKE}\""
 echo "RELEASE is: \"${RELEASE}\""
 echo "PTB is: \"${PTB}\""
-echo "VERSION is: \"${VERSION}\""
+echo "LINUXDEPLOY_OUTPUT_VERSION is: \"${LINUXDEPLOY_OUTPUT_VERSION}\""
 echo "OUTPUT_NAME is: \"${OUTPUT_NAME}\""
 
-# clean up the build/ folder of intermediate files
-rm -rf "${BUILD_DIR}"/*.o
-rm -rf "${BUILD_DIR}"/*.cpp
-rm -rf "${BUILD_DIR}"/*.h
-#mkdir -p "${BUILD_DIR}"
-
-# delete previous appimage
+# delete any previous appimage
 rm -f Mudlet*.AppImage
 
-# move the binary up to the build folder (they differ between qmake and cmake,
+# clean up the "${APP_DIR}"/ folder of any previous files:
+rm -rf "${APP_DIR}"/
+
+# make some directories we'll need:
+# For the mudlet executable:
+mkdir -p "${APP_DIR}"/usr/bin/
+# For the libraries:
+mkdir -p "${APP_DIR}"/usr/lib/
+# For the lua files - not needed as the translations one will make it:
+# mkdir -p "${APP_DIR}"/usr/share/applications/mudlet/lua
+# For the lua code formatter files:
+mkdir -p "${APP_DIR}"/usr/share/applications/mudlet/lcf
+# For the lua translations
+mkdir -p "${APP_DIR}"/usr/share/applications/mudlet/lua/translations
+# For the dictionaries we ship as a fallback
+mkdir -p "${APP_DIR}"/usr/share/hunspell
+
+
+# copy the binary into the AppDir folders (they differ between qmake and cmake,
 # so we use find to find the binary
-find "${BUILD_DIR}"/ -iname mudlet -type f -exec cp '{}' "${BUILD_DIR}"/ \;
-# get mudlet-lua in there as well so linuxdeployqt bundles it
-cp -rf "${SOURCE_DIR}"/src/mudlet-lua "${BUILD_DIR}"/
-# copy Lua translations
-# only copy if folder exists
-mkdir -p "${BUILD_DIR}"/translations/lua
-[ -d "${SOURCE_DIR}"/translations/lua ] && cp -rf "${SOURCE_DIR}"/translations/lua "${BUILD_DIR}"/translations/
+find "${BUILD_DIR}"/ -iname mudlet -type f -exec cp '{}' "${APP_DIR}"/usr/bin \;
+# get the lua files in:
+cp -rf "${SOURCE_DIR}"/src/mudlet-lua/lua "${APP_DIR}"/usr/share/applications/mudlet
+# get the lua code formatter files in:
+cp -rf "${SOURCE_DIR}"/3rdparty/lcf "${APP_DIR}"/usr/share/applications/mudlet
+# copy Lua translations only copy if folder exists
+[ -d "${SOURCE_DIR}"/translations/lua ] && cp -f "${SOURCE_DIR}"/translations/lua/translated/*.json "${APP_DIR}"/usr/share/applications/mudlet/lua/translations
+
 # and the dictionary files in case the user system doesn't have them (at a known
 # place)
-cp "$SOURCE_DIR"/src/*.dic "${BUILD_DIR}"/
-cp "$SOURCE_DIR"/src/*.aff "${BUILD_DIR}"/
-# and the .desktop and files used for icons so linuxdeploy can pilfer them
-cp "$SOURCE_DIR"/mudlet{.desktop,.png,.svg} "${BUILD_DIR}"/
+cp "${SOURCE_DIR}"/src/*.dic "${APP_DIR}"/usr/share/hunspell
+cp "${SOURCE_DIR}"/src/*.aff "${APP_DIR}"/usr/share/hunspell
 
-cp -r "$SOURCE_DIR"/3rdparty/lcf "${BUILD_DIR}"/
+# and the .desktop and files used for icons so linuxdeploy can pilfer them
+cp "${SOURCE_DIR}"/mudlet{.desktop,.png,.svg} "${APP_DIR}"/usr/share/applications/mudlet
 
 # now copy Lua modules we need in
-# this should be improved not to be hardcoded
-mkdir -p "${BUILD_DIR}"/lib/luasql
-mkdir -p "${BUILD_DIR}"/lib/brimworks
-
-cp "${SOURCE_DIR}"/3rdparty/discord/rpc/lib/libdiscord-rpc.so "${BUILD_DIR}"/lib/
+mkdir -p "${APP_DIR}"/usr/lib/luasql
+mkdir -p "${APP_DIR}"/usr/lib/brimworks
 
 for LIB in lfs rex_pcre luasql/sqlite3 brimworks/zip lua-utf8 yajl
 do
@@ -134,9 +172,13 @@ do
   for LIB_PATH in $(luarocks path --lr-cpath | tr ";" "\n")
   do
     CHANGED_PATH=${LIB_PATH/\?/${LIB}};
+    echo "For \"${LIB}\" changing path from \"${LIB_PATH}\" to \"${CHANGED_PATH}\"."
     if [ -e "${CHANGED_PATH}" ]; then
-      cp -rL "${CHANGED_PATH}" "${BUILD_DIR}"/lib/${LIB}.so
-      FOUND="true"
+      # For previous linuxdeployqt
+      # cp -rL "${CHANGED_PATH}" "${BUILD_DIR}"/lib/${LIB}.so
+      if cp -vrL "${CHANGED_PATH}" "${APP_DIR}/usr/lib/${LIB}.so"; then
+        FOUND="true"
+      fi
     fi
   done
   if [ "${FOUND}" == "false" ]; then
@@ -145,9 +187,8 @@ do
   fi
 done
 
-# extract linuxdeployqt since some environments (like travis) don't allow FUSE
-#./linuxdeploy.AppImage --appimage-extract
-
+# Discord library
+cp -v "${SOURCE_DIR}"/3rdparty/discord/rpc/lib/libdiscord-rpc.so "${APP_DIR}"/usr/lib/
 
 # QMAKE is an extra detail (path and filename of the qmake to use) needed for
 # the qt plugin - including selecting a Qt 6 make which should have been done
@@ -159,31 +200,51 @@ export QT_INSTALL_PLUGINS
 
 # In case we need any:
 EXTRA_QT_MODULES=""
-echo "Using extra modules: ${EXTRA_QT_MODULES}"
+echo "Using extra modules: \"${EXTRA_QT_MODULES}\""
 export EXTRA_QT_MODULES
 
 # Bundle libssl.so so Mudlet works on platforms that only distribute
 # OpenSSL 1.1
-cp -L /usr/lib/x86_64-linux-gnu/libssl.so* "${BUILD_DIR}"/lib/ 2>/dev/null || true
-cp -L /lib/x86_64-linux-gnu/libssl.so* "${BUILD_DIR}"/lib/ 2>/dev/null || true
-if [ -z "$(ls "${BUILD_DIR}"/lib/libssl.so*)" ]; then
-  echo "No OpenSSL libraries to copy found. Aborting..."
-  exit 1
+cp -Lv /usr/lib/x86_64-linux-gnu/libssl.so* "${BUILD_DIR}"/lib/ 2>/dev/null || true
+cp -Lv /lib/x86_64-linux-gnu/libssl.so* "${BUILD_DIR}"/lib/ 2>/dev/null || true
+if [ -z "$(ls "${BUILD_DIR}"/lib/libssl.so* 2>/dev/null)" ]; then
+  echo "No OpenSSL libraries to copy found. This might be a problem..."
+  # exit 1
 fi
 
 echo "Generating AppImage"
+# Note: the gstreamer plugin needs the patchelf utility!
+./linuxdeploy.AppImage --appdir "${APP_DIR}" --plugin qt --plugin gstreamer --icon-file "${SOURCE_DIR}"/mudlet.svg --desktop-file "${SOURCE_DIR}"/mudlet.desktop --executable "${APP_DIR}"/usr/bin/mudlet --output appimage
 
-# Leaving out --plugin gstreamer  for the moment
-./linuxdeploy.AppImage --appdir "${APP_DIR}" --plugin qt --output appimage --icon-file "${BUILD_DIR}"/mudlet.svg --desktop-file "${BUILD_DIR}"/mudlet.desktop --executable "${BUILD_DIR}"/mudlet
+# Unfortunately, until https://github.com/linuxdeploy/linuxdeploy-plugin-qt/issues/194
+# is resolved we have to go through the ${APP_DIR}/usr/translations/ directory
+# and combine the qtbase_xx(_YY).qm and qtmultimedia_xx(_YY).qm files into
+# a single qt_xx(_YY).qm file with a:
+# "lconvert -o ./qt_xx(_YY).qm qtbase_xx(_YY).qm qtmultimedia_xx(_YY).qm"
+# The '_' in workarea needs to be accounted for in the cut fields to index
+for baseFile in "${APP_DIR}"/usr/translations/qtbase_*.qm
+do
+    localeCode=$(echo "${baseFile}" | cut -d_ -f 2-)
+    # remember localeCode includes the ".qm" extension
+    if [ -f "${APP_DIR}"/usr/translations/qtmultimedia_${localeCode} ]; then
+        # This locale DOES have a qtmultimedia_*.qm file so include it:
+        lconvert -o "${APP_DIR}"/usr/translations/qt_${localeCode} "${APP_DIR}"/usr/translations/qtbase_${localeCode} "${APP_DIR}"/usr/translations/qtmultimedia_${localeCode}
+    else
+        # It doesn't so just convert/copy the base one:
+        lconvert -o "${APP_DIR}"/usr/translations/qt_${localeCode} "${APP_DIR}"/usr/translations/qtbase_${localeCode}
+    fi
+done
 
-#./squashfs-root/AppRun ./build/mudlet -appimage \
-#  -executable=build/lib/rex_pcre.so -executable=build/lib/zip.so \
-#  -executable=build/lib/luasql/sqlite3.so -executable=build/lib/yajl.so \
-#  -executable=build/lib/libssl.so.1.1 \
-#  -executable=build/lib/libssl.so.1.0.0 \
-#  -extra-plugins=texttospeech/libqttexttospeech_flite.so,texttospeech/libqttexttospeech_speechd.so,platforminputcontexts/libcomposeplatforminputcontextplugin.so,platforminputcontexts/libibusplatforminputcontextplugin.so,platforminputcontexts/libfcitxplatforminputcontextplugin.so
+# Remove the individual files
+rm "${APP_DIR}"/usr/translations/qtbase_*.qm
+rm "${APP_DIR}"/usr/translations/qtmultimedia_*.qm
 
-# clean up extracted appimage
-#rm -rf squashfs-root/
+# Remove some files from prior run that will be regenerated - and give us a
+# warning if not removed
+rm "${APP_DIR}"/AppRun
+rm "${APP_DIR}"/AppRun.wrapped
+
+# Rerun the base linuxdeploy to regenerate a new AppImage
+./linuxdeploy.AppImage --appdir "${APP_DIR}" --output appimage
 
 mv Mudlet*.AppImage "${OUTPUT_NAME}.AppImage"
